eff28707430df11f17252b8b3ce24aed9068d878
sajid4kgh <160305227+sajid4kgh@users.noreply.github.com>
2024-04-22 13:55:06 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// My first program in Typescript
+console.log("Hello World.!");
+let ans = true;
+let num = 0;
+let table = 5;
+while (num <= 10) {
+    console.log(table, '*', num, '=', num * table);
+    num++;
+}
---
+++ hello.ts
@@ -0,0 +1,12 @@
+// My first program in Typescript
+console.log("Hello World.!");
+let ans = true;
+let num=0;
+let table=5;
+
+while(num<=10){
+      console.log(table, '*', num, '=', num*table);
+      num++;
+  }
+
+
